b420927f2371d5e031575240567ae862487cb9b4
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-20 08:51:00 -0700

    added media controller

--- app/src/main/java/com/example/clarkhinchcliff/moviedemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/moviedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.clarkhinchcliff.moviedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mediaController = new MediaController(this);
 
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.test);
+
+//        set up media controller on video
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
 
         videoView.start();
     }
