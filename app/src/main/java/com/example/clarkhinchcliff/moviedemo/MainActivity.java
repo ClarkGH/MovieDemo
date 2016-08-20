58e23c601a869267a29dcf640494757573ec8ed3
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2016-08-20 08:37:34 -0700

    video's connected

--- app/src/main/java/com/example/clarkhinchcliff/moviedemo/MainActivity.java
+++ app/src/main/java/com/example/clarkhinchcliff/moviedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.clarkhinchcliff.moviedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.drawable.SampleVideo_640x360_1mb);
+
+        videoView.start();
     }
 }
